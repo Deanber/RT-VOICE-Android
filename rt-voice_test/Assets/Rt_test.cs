7945fba3b7431a79c435c3aa53411934454d8cd6
Deanber <2511688525@qq.com>
2024-10-18 23:17:33 +0800

    Initial commit

---
+++ rt-voice_test/Assets/Rt_test.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Crosstales.RTVoice;
+using Crosstales.RTVoice.Model;
+using TMPro;
+
+public class Rt_test : MonoBehaviour
+{
+    public string Text;
+    public string Culture = "en";
+    public bool SpeakWhenReady;
+
+    TMP_InputField ti;
+
+    private string uid; //Unique id of the speech
+
+    void Start()
+    {
+        ti = this.GetComponent<TMP_InputField>();
+    }
+    void Update()
+    {
+        Text = ti.text;
+    }
+
+
+
+    private void OnEnable()
+    {
+        // Subscribe event listeners
+        Speaker.Instance.OnVoicesReady += voicesReady;
+        Speaker.Instance.OnSpeakStart += speakStart;
+        Speaker.Instance.OnSpeakComplete += speakComplete;
+    }
+
+    private void OnDisable()
+    {
+        if (Speaker.Instance != null)
+        {
+            // Unsubscribe event listeners
+            Speaker.Instance.OnVoicesReady -= voicesReady;
+            Speaker.Instance.OnSpeakStart -= speakStart;
+            Speaker.Instance.OnSpeakComplete -= speakComplete;
+        }
+    }
+
+    public void Speak()
+    {
+        uid = Speaker.Instance.Speak(Text, null, Speaker.Instance.VoiceForCulture(Culture)); //Speak with the first voice matching the given culture
+    }
+
+    private void voicesReady()
+    {
+        Debug.Log($"RT-Voice: {Speaker.Instance.Voices.Count} voices are ready to use!");
+
+        if (SpeakWhenReady) //Speak after the voices are ready
+            Speak();
+    }
+
+    private void speakStart(Wrapper wrapper)
+    {
+        if (wrapper.Uid == uid) //Only write the log message if it's "our" speech
+            Debug.Log($"RT-Voice: speak started: {wrapper}");
+    }
+
+    private void speakComplete(Wrapper wrapper)
+    {
+        if (wrapper.Uid == uid) //Only write the log message if it's "our" speech
+            Debug.Log($"RT-Voice: speak completed: {wrapper}");
+    }
+}
